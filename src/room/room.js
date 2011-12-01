7d6f442510de43d40b5a20ba9d59f883ba183f59
Anders Edqvist <anders.edqvist@gmail.com>
2011-12-01 23:13:38 +0100

    Room class
    Subclasses are responsible for setting "backgroundImage" to something appropriate.

--- src/room/room.js
+++ src/room/room.js
@@ -1,3 +1,5 @@
+var gamejs = require('gamejs');
+
 /*
  * Room class
  * 
@@ -18,3 +20,24 @@
  *  init
  *  enter(sprite, direction)
  */
+
+var Room = exports.Room = function() {
+    this.backgroundImage = null;
+    this.positionOnMap = [0, 0];
+    this.updateables = new gamejs.sprite.Group();
+    this.drawables = new gamejs.sprite.Group();
+    return this;
+};
+
+Room.prototype.draw = function(surface) {
+    if (this.backgroundImage) {
+	 surface.blit(this.backgroundImage);
+    } else {
+	 surface.fill("purple");
+    }
+    this.drawables.draw.apply(this.drawables, arguments);
+};
+
+Room.prototype.update = function(msduration) {
+    this.updateables.update.apply(this.updateables, arguments);
+};
