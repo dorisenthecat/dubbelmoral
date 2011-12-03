c01aeb2ffa283256ddb17783c30454902a0ea1e1
Albin Axelsson <albinaxelsson@gmail.com>
2011-12-03 11:12:25 +0100

    Reduced use of magic numbers in the bottle placement algorithm.

--- src/room/nilen.js
+++ src/room/nilen.js
@@ -4,15 +4,25 @@ var beer = require('../sprite/beer');
 
 gamejs.preload(["images/nilen.png"]);
 
+//Constants
+var GROUND_HEIGHT = 2;
+var MAX_BOTTLES = 4;
+
 var Nilen = exports.Nilen = function() {
 	Nilen.superConstructor.apply(this, arguments);
 	
 	//Add beer bottles
-	var numberOfBeer = Math.floor(Math.random()*5); // 0-4 bottles
+	var numberOfBeer = Math.floor(Math.random()*MAX_BOTTLES+1);
 	for(var i=0; i<numberOfBeer; i++) {
-		var x = Math.floor(Math.random()*182); //Place bottle somewhere between 0 and 181
+		var bottle = new beer.Beer();
+		
+		//Place bottle somewhere on the ground
+		bottle.rect.left = Math.floor(Math.random()*gamejs.display.getSurface().getSize()[0] - bottle.rect.width);
+		bottle.rect.top = Math.floor(gamejs.display.getSurface().getSize()[1] - bottle.rect.height - GROUND_HEIGHT);
+					
 		//TODO: Check if a bottle already exists in this position
-		this.drawables.add(new beer.Beer([x,175]));  //TODO: Do not hard code Y
+		
+		this.drawables.add(bottle);
 	}
 	    
     this.backgroundImage = gamejs.image.load("images/nilen.png"); // todo: load once, not per object
--- src/sprite/beer.js
+++ src/sprite/beer.js
@@ -3,13 +3,10 @@ var gamejs = require('gamejs');
 gamejs.preload(["images/beer.png"]);  //TODO: Should load from sprites.png?
 
 
-/**
- * \param position array of size 2 containing the position of the beerbottle
- */
-var Beer = exports.Beer = function (position) {
+var Beer = exports.Beer = function () {
 	Beer.superConstructor.apply(this, arguments);
 	
-	this.rect = position;
+	this.rect = new gamejs.Rect([0,0], [4,8]);      //TODO: How to get size of sprite?
 	this.image = gamejs.image.load("images/beer.png"); //TODO: Should be done once, not per instance
 	
 	return this;
