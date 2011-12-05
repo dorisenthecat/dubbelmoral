d8560118dbdfa8d1c224ad5c0317020426f96d14
Anders Edqvist <anders.edqvist@gmail.com>
2011-12-05 17:28:12 +0100

    Added Map and Hemma canvas and sample paint calls.

--- src/dubbelmoral.js
+++ src/dubbelmoral.js
@@ -16,9 +16,18 @@ var nilen = require('./room/nilen');
  *  map (Map)
  */
 
+gamejs.preload(["images/map.png"]);
+gamejs.preload(["images/hemma.png"]);
+
 function main() {
     var display = gamejs.display.setMode([326, 185]);
     var currentRoom = new nilen.Nilen();
+    
+    var mapDisplay = gamejs.display.setMode([148,299], "map");
+    mapDisplay.blit(gamejs.image.load("images/map.png"));
+    
+    var hemmaDisplay = gamejs.display.setMode([326, 64], "hemma");
+    hemmaDisplay.blit(gamejs.image.load("images/hemma.png"));
     
     currentRoom.draw(display);
 };
