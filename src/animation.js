92998a7dfca86c71bad199494c5e2f4f04a6de64
Anders Edqvist <anders.edqvist@gmail.com>
2011-12-06 22:54:00 +0100

    Animation foundation
    It is possible to reuse an object as an animation cursor (useful for sprites).

--- src/animation.js
+++ src/animation.js
@@ -5,17 +5,54 @@
  *  frames (Array)
  * 
  * Methods
- *  startAnimation(animationCursor)
+ *  startAnimation(animationCursor, finished)
  */
+
+var Animation = exports.Animation = function() {
+    this.frames = [];
+};
+
+Animation.prototype.start = function(cursor, finished) {
+    return initCursor(cursor || {}, this, finished);
+};
 
 /*
  * AnimationFrame
+ *
+ * Members
+ *  ticks
  */
+var AnimationFrame = exports.AnimationFrame = function() {
+};
 
 /*
- * AnimationCursor (inteface?)
+ * AnimationCursor (interface)
  * 
- * Members
- *  currentFrame
- *  elapsedTicks
+ * Methods
+ *  updateAnimation()
+ *  isAnimationFinished()
  */
+function initCursor(cursor, animation, finished) {
+    var _currentFrame = 0;
+    var _elapsedTicks = 0;
+    var _animation = animation;
+    var _finished = finished;
+    cursor.updateAnimation = function () {
+	 var done;
+	 _elapsedTicks++;
+	 if (_currentFrame < _animation.frames.length) {
+	     if (_elapsedTicks >= _animation.frames[_currentFrame].ticks) {
+		  _currentFrame++;
+		  _elapsedTicks = 0;
+	     }
+	 }
+	 done = (_currentFrame >= _animation.frames.length);
+	 if (done && _finished) {
+	     _finished(_animation);
+	 }
+    };
+    cursor.isAnimationFinished = function() {
+	 return (_currentFrame >= _animation.frames.length);
+    };
+    return cursor;
+};
--- src/dubbelmoral.js
+++ src/dubbelmoral.js
@@ -3,6 +3,8 @@ var gamejs = require('gamejs');
 var nilen = require('./room/nilen');
 var Map = require("./map").Map;
 
+var animation = require("./animation");
+
 /*
  * Game
  * 
@@ -30,6 +32,14 @@ function main() {
     hemmaDisplay.blit(gamejs.image.load("images/hemma.png"));
     map.draw(mapDisplay);
     currentRoom.draw(display);
+    
+    var anim = new animation.Animation();
+    var fr = new animation.AnimationFrame();
+    fr.ticks = 2;
+    anim.frames.push(fr);
+    var cursor = anim.start(this, function () { console.log("animation completed"); });
+    this.updateAnimation();
+    cursor.updateAnimation();
 };
 
 gamejs.ready(function() {
