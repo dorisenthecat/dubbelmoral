d06b4a0a0437c64834a58e4ec569b79df9a82f91
Albin Axelsson <albinaxelsson@gmail.com>
2011-12-10 19:40:17 +0100

    Cleanup/whitespace

--- src/sprite/branch.js
+++ src/sprite/branch.js
@@ -4,24 +4,23 @@ var spriteanimation = require("./animatedsprite");
 gamejs.preload(["images/branch/branch_right_1.png"]);
 gamejs.preload(["images/branch/branch_right_2.png"]);
 gamejs.preload(["images/branch/branch_right_3.png"]);
-//, "images/branch/branch_right_2.png", "images/branch/branch_right_3.png"]);
 
 var Branch = exports.Branch = function() {
     Branch.superConstructor.apply(this, arguments);
     var standAnimation = new spriteanimation.SpriteAnimation(
-	 {
-	     frames: [
-		  {ticks: 1, image: "images/branch/branch_right_1.png", vy: 5},
-		  {ticks: 1, image: "images/branch/branch_right_2.png", vy: 5},
-		  {ticks: 1, image: "images/branch/branch_right_3.png", vy: 5},
-	     ]
-	 }
+      {
+        frames: [
+        {ticks: 1, image: "images/branch/branch_right_1.png", vy: 5},
+        {ticks: 1, image: "images/branch/branch_right_2.png", vy: 5},
+        {ticks: 1, image: "images/branch/branch_right_3.png", vy: 5},   
+        ]
+      }
     );
     
     var self = this;
     standAnimation.start(this, function(anim) {
-				 self.restartAnimation();
-			    });
+      self.restartAnimation();
+    });
     return this;
 };
 gamejs.utils.objects.extend(Branch, spriteanimation.AnimatedSprite);
@@ -33,13 +32,13 @@ Branch.prototype.update = function(msduration, context) {
     
     var self = this;
     if (gamejs.sprite.collideRect(this, context.student)) {
-	 hitStudent = true;
-	 //TODO: set animation
+       hitStudent = true;
+       //TODO: set animation
     }
 
     if(this.rect.top > 185 || hitStudent) {
-	 this.rect.bottom = 0;
-	 this.updatePosition();
+      this.rect.bottom = 0;
+      this.updatePosition();
     }
     
 };
