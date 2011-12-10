20e313bd11e6ffe219244d534833a479eb13d995
Albin Axelsson <albinaxelsson@gmail.com>
2011-12-10 20:08:12 +0100

    Cleanup of room Fontänen

--- src/room/fontanen.js
+++ src/room/fontanen.js
@@ -12,32 +12,10 @@ var GROUND_HEIGHT = 2;
 var MAX_BOTTLES = 4;
 
 var Fontanen = exports.Fontanen = function() {
-	Fontanen.superConstructor.apply(this, arguments);
-	
-	var b = new branch.Branch([100,50]);
-	
-	this.drawables.add(b);
-	this.updateables.add(b);
-	
-	//Add beer bottles
-	var numberOfBeer = Math.floor(Math.random()*MAX_BOTTLES+1);
-	for(var i=0; i<numberOfBeer; i++) {
-		var bottle = new beer.Beer();
-		
-		//Place bottle somewhere on the ground
-		bottle.rect.left = Math.floor(Math.random()*gamejs.display.getSurface().getSize()[0] - bottle.rect.width);
-		bottle.rect.top = Math.floor(gamejs.display.getSurface().getSize()[1] - bottle.rect.height - GROUND_HEIGHT);
-		
-		//TODO: Check if a bottle already exists in this position
-		
-		this.drawables.add(bottle);
-	}
-	
-	//Add ladder to the tree
-	this.ladders.add(new ladder.Ladder(new gamejs.Rect([215,88], [12,95])));
-	    
-    this.backgroundImage = gamejs.image.load("images/fontanen.png"); // todo: load once, not per object
-    
-    return this;
+  Fontanen.superConstructor.apply(this, arguments);
+
+  this.backgroundImage = gamejs.image.load("images/fontanen.png"); // todo: load once, not per object
+  
+  return this;
 };
 gamejs.utils.objects.extend(Fontanen, room.Room);
